be2112e28134b8329692bc573293d3807552f931
Curios Ri <curiosri@gmail.com>
2022-12-25 23:57:07 +0900

    handling validation error with strings

--- client/src/app/api/agent.ts
+++ client/src/app/api/agent.ts
@@ -15,9 +15,21 @@ axios.interceptors.response.use(response => {
     const {data, status} : any = error.response!;
     switch (status) {
         case 400:
+            if (data.errors) {
+                const modelStateErrors: string[] = [];
+                for (const key in data.errors) {
+                    if (data.errors[key]) {
+                        modelStateErrors.push(data.errors[key])
+                    }
+                }
+                throw modelStateErrors.flat();
+            }
             toast.error(data.title);
             break;
         case 401:
+            toast.error(data.title);
+            break;
+        case 404:
             toast.error(data.title);
             break;
         case 500:
